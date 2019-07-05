6ff8bc0b126fb12af30e6ab4fd0b1dc5669d79a3
Karen Fletcher <knpfletcher@gmail.com>
2019-07-05 16:06:31 -0500

    Refactor jquery
    Reduce repeated code by listening for click events on chevron buttons, finding closest ancestor to clicked button and hidden view, toggle that view. all other views with matching class name, if visible, slide up.

--- js/app.js
+++ js/app.js
@@ -1,38 +1,7 @@
-$(document).ready(function() {
+$(document).click(".person__details", function(event) {
 
-  $("#person0-anchor").click(function() {
-    $("#person1-details").slideUp($(this).is(':visible'));
-    $("#person2-details").slideUp($(this).is(':visible'));
-    $("#person3-details").slideUp($(this).is(':visible'));
-    $("#person0-details").slideToggle("fast", function() {
-      $("#person0-details").toggleClass("hidden");
-    });
-  });
-
-  $("#person1-anchor").click(function() {
-    $("#person0-details").slideUp($(this).is(':visible'));
-    $("#person2-details").slideUp($(this).is(':visible'));
-    $("#person3-details").slideUp($(this).is(':visible'));
-    $("#person1-details").slideToggle("fast", function() {
-      $("#person1-details").toggleClass("hidden");
-    });
-  });
-
-  $("#person2-anchor").click(function() {
-    $("#person0-details").slideUp($(this).is(':visible'));
-    $("#person1-details").slideUp($(this).is(':visible'));
-    $("#person3-details").slideUp($(this).is(':visible'));
-    $("#person2-details").slideToggle("fast", function() {
-      $("#person2-details").toggleClass("hidden");
-    });
-  });
-
-  $("#person3-anchor").click(function() {
-    $("#person0-details").slideUp($(this).is(':visible'));
-    $("#person1-details").slideUp($(this).is(':visible'));
-    $("#person2-details").slideUp($(this).is(':visible'));
-    $("#person3-details").slideToggle("fast", function() {
-      $("#person3-details").toggleClass("hidden");
-    });
-  });
+  detailsToOpen = $(event.target).closest(".m-people__person").find(".contact__details");
+  detailsToClose = $(".contact__details:visible").not(detailsToOpen)
+  detailsToClose.slideUp();
+  detailsToOpen.slideToggle();
 });
